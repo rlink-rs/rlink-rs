9ab1554e19a0763c19b70fad138d133ee57c4366
yorkart <wangyue11.4@163.com>
2021-01-04 11:55:53 +0800

    virtual io

--- rlink/src/dag/mod.rs
+++ rlink/src/dag/mod.rs
@@ -3,15 +3,58 @@
 
 use std::error::Error;
 
-use crate::api::operator::FunctionCreator;
+use crate::api::operator::{FunctionCreator, StreamOperatorWrap};
 
-pub(crate) mod execution_graph;
-pub(crate) mod job_graph;
+// pub(crate) mod execution_graph;
+// pub(crate) mod job_graph;
 pub(crate) mod stream_graph;
+pub(crate) mod virtual_io;
 
+use std::borrow::BorrowMut;
+use std::ops::{Deref, DerefMut};
 pub(crate) use stream_graph::StreamGraph;
 
-pub(crate) enum OperatorType {
+#[derive(Copy, Clone, Serialize, Deserialize, Debug)]
+pub(crate) enum ParValueType {
+    /// by user custom
+    Custom,
+    /// inherit from parent node
+    Inherit,
+}
+
+#[derive(Copy, Clone, Serialize, Deserialize, Debug)]
+pub(crate) struct Parallelism {
+    value: u32,
+    value_type: ParValueType,
+}
+
+impl Parallelism {
+    pub fn new(value: u32) -> Self {
+        let value_type = if value == 0 {
+            ParValueType::Inherit
+        } else {
+            ParValueType::Custom
+        };
+        Parallelism { value, value_type }
+    }
+}
+
+impl Deref for Parallelism {
+    type Target = u32;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl DerefMut for Parallelism {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.value.borrow_mut()
+    }
+}
+
+#[derive(Clone, Copy, Serialize, Deserialize, Debug)]
+pub enum OperatorType {
     Source,
     Map,
     Filter,
@@ -23,10 +66,45 @@ pub(crate) enum OperatorType {
     Sink,
 }
 
+impl<'a> From<&'a StreamOperatorWrap> for OperatorType {
+    fn from(op: &'a StreamOperatorWrap) -> Self {
+        match op {
+            StreamOperatorWrap::StreamSource(_) => OperatorType::Source,
+            StreamOperatorWrap::StreamMap(_) => OperatorType::Map,
+            StreamOperatorWrap::StreamFilter(_) => OperatorType::Filter,
+            StreamOperatorWrap::StreamCoProcess(_) => OperatorType::CoProcess,
+            StreamOperatorWrap::StreamKeyBy(_) => OperatorType::KeyBy,
+            StreamOperatorWrap::StreamReduce(_) => OperatorType::Reduce,
+            StreamOperatorWrap::StreamWatermarkAssigner(_) => OperatorType::WatermarkAssigner,
+            StreamOperatorWrap::StreamWindowAssigner(_) => OperatorType::WindowAssigner,
+            StreamOperatorWrap::StreamSink(_) => OperatorType::Sink,
+        }
+    }
+}
+
+impl std::fmt::Display for OperatorType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            OperatorType::Source => write!(f, "Source"),
+            OperatorType::Map => write!(f, "Map"),
+            OperatorType::Filter => write!(f, "Filter"),
+            OperatorType::CoProcess => write!(f, "CoProcess"),
+            OperatorType::KeyBy => write!(f, "KeyBy"),
+            OperatorType::Reduce => write!(f, "Reduce"),
+            OperatorType::WatermarkAssigner => write!(f, "WatermarkAssigner"),
+            OperatorType::WindowAssigner => write!(f, "WindowAssigner"),
+            OperatorType::Sink => write!(f, "Sink"),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum DagError {
-    ChildNodeNotFound(String),
+    SourceNotFound,
+    SinkNotFound,
+    ChildNodeNotFound(OperatorType),
     ParentOperatorNotFound,
+    ParallelismInheritUnsupported(OperatorType),
 }
 
 impl Error for DagError {}
@@ -34,8 +112,13 @@ impl Error for DagError {}
 impl std::fmt::Display for DagError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            DagError::SourceNotFound => write!(f, "SourceNotFound"),
+            DagError::SinkNotFound => write!(f, "SinkNotFound"),
             DagError::ChildNodeNotFound(s) => write!(f, "ChildNodeNotFound({})", s),
             DagError::ParentOperatorNotFound => write!(f, "ParentOperatorNotFound"),
+            DagError::ParallelismInheritUnsupported(s) => {
+                write!(f, "ParallelismInheritUnsupported({})", s)
+            }
         }
     }
 }
@@ -43,10 +126,10 @@ impl std::fmt::Display for DagError {
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct StreamNode {
     id: u32,
-    parallelism: u32,
+    parallelism: Parallelism,
 
     operator_name: String,
-
+    operator_type: OperatorType,
     fn_creator: FunctionCreator,
 }
 
--- rlink/src/dag/stream_graph.rs
+++ rlink/src/dag/stream_graph.rs
@@ -1,10 +1,10 @@
-use std::collections::{HashMap, HashSet};
-use std::ops::{Index, IndexMut};
+use std::collections::HashMap;
+use std::ops::Index;
 
-use daggy::{Dag, EdgeIndex, NodeIndex, Walker};
+use daggy::{Dag, EdgeIndex, NodeIndex};
 
 use crate::api::operator::{StreamOperatorWrap, TStreamOperator};
-use crate::dag::{DagError, StreamEdge, StreamNode};
+use crate::dag::{DagError, OperatorType, Parallelism, StreamEdge, StreamNode};
 
 #[derive(Debug)]
 pub(crate) struct StreamGraph {
@@ -39,14 +39,31 @@ impl StreamGraph {
         &self.dag
     }
 
+    // pub fn index(&self, node_index: NodeIndex) -> StreamNode {
+    //     self.dag.index(node_index).clone()
+    // }
+
     pub fn add_operator(&mut self, operator: StreamOperatorWrap) -> Result<(), DagError> {
         let operator_id = operator.get_operator_id();
         let parent_operator_ids = operator.get_parent_operator_ids();
 
+        // let parallelism = Parallelism::new(operator.get_parallelism());
+        // if parallelism.value_type == ParValueType::Inherit {
+        //     let max_parallelism = parent_operator_ids
+        //         .iter()
+        //         .map(|p_id| self.operators.get(p_id).unwrap().0)
+        //         .map(|p_node_index| self.dag.index(p_node_index))
+        //         .map(|p_node_stream| p_node_stream.parallelism)
+        //         .max_by_key(|p_parallelism| p_parallelism.value)
+        //         .expect("can not inherit parallelism");
+        //     *parallelism = *max_parallelism
+        // }
+
         let stream_node = StreamNode {
             id: operator_id,
-            parallelism: operator.get_parallelism(),
+            parallelism: Parallelism::new(operator.get_parallelism()),
             operator_name: operator.get_operator_name().to_string(),
+            operator_type: OperatorType::from(&operator),
             fn_creator: operator.get_fn_creator(),
         };
 
@@ -85,77 +102,111 @@ impl StreamGraph {
         Ok(())
     }
 
-    fn parallelism_analyse(&mut self, begin_node_indies: Vec<NodeIndex>) -> Result<(), DagError> {
-        // parse from sources and break with meeting node
-        let mut meeting_node_set = HashSet::new();
-        for begin_node_index in begin_node_indies {
-            let meeting_node_index = self.pipeline_parse(begin_node_index)?;
-            meeting_node_set.insert(meeting_node_index);
-        }
-
-        for meeting_node_index in meeting_node_set {
-            let meeting_parallelism = self.dag.index(meeting_node_index).parallelism;
-            if meeting_parallelism == 0 {
-                // inherited the max(parent parallelism).
-                // that is meeting node's parallelism == max(parent parallelism)
-
-                let parents: Vec<(EdgeIndex, NodeIndex)> = self
-                    .dag
-                    .parents(meeting_node_index)
-                    .iter(&self.dag)
-                    .collect();
-
-                let max_parallelism = parents
-                    .into_iter()
-                    .map(|(_edge_index, node_index)| self.dag.index(node_index).parallelism)
-                    .max()
-                    .unwrap();
-
-                self.dag.index_mut(meeting_node_index).parallelism = max_parallelism;
-            }
-
-            // go on after meeting's pipeline parse
-            self.parallelism_analyse(vec![meeting_node_index])?;
-        }
-
-        Ok(())
-    }
-
-    fn pipeline_parse(&mut self, node_index: NodeIndex) -> Result<NodeIndex, DagError> {
-        let stream_node = self.dag.index(node_index).clone();
-        let children: Vec<(EdgeIndex, NodeIndex)> =
-            self.dag.children(node_index).iter(&self.dag).collect();
-        let operator = {
-            let n = self.operators.get(&stream_node.id).unwrap();
-            &n.1
-        };
-
-        if children.len() == 0 && !operator.is_sink() {
-            return Err(DagError::ChildNodeNotFound(
-                stream_node.operator_name.clone(),
-            ));
-        }
-
-        if children.len() == 1 {
-            let child = children.get(0).unwrap();
-            let parents: Vec<(EdgeIndex, NodeIndex)> =
-                self.dag.parents(child.1).iter(&self.dag).collect();
-
-            return if parents.len() == 1 {
-                let child_stream_index = self.dag.index_mut(child.1);
-                if child_stream_index.parallelism == 0 {
-                    child_stream_index.parallelism = stream_node.parallelism;
-                }
-                self.pipeline_parse(child.1)
-            } else {
-                Ok(child.1)
-            };
-        }
-
-        if children.len() > 1 {
-            unimplemented!()
-        }
-
-        unimplemented!()
-    }
+    // fn parallelism_analyse(&mut self, begin_node_indies: Vec<NodeIndex>) -> Result<(), DagError> {
+    //     // parse from sources and break with meeting node
+    //     let mut meeting_node_set = HashSet::new();
+    //     for begin_node_index in begin_node_indies {
+    //         let meeting_node_index = self.pipeline_parse(begin_node_index)?;
+    //         meeting_node_set.insert(meeting_node_index);
+    //     }
+    //
+    //     for meeting_node_index in meeting_node_set {
+    //         let meeting_parallelism = self.dag.index(meeting_node_index).parallelism;
+    //         if *meeting_parallelism == 0 {
+    //             // inherited the max(parent parallelism).
+    //             // that is meeting node's parallelism == max(parent parallelism)
+    //
+    //             let parents: Vec<(EdgeIndex, NodeIndex)> = self
+    //                 .dag
+    //                 .parents(meeting_node_index)
+    //                 .iter(&self.dag)
+    //                 .collect();
+    //
+    //             let max_parallelism = parents
+    //                 .into_iter()
+    //                 .map(|(_edge_index, node_index)| self.dag.index(node_index).parallelism)
+    //                 .max_by_key(|p| p.value)
+    //                 .unwrap();
+    //
+    //             self.dag.index_mut(meeting_node_index).parallelism = max_parallelism;
+    //         }
+    //
+    //         // go on after meeting's pipeline parse
+    //         self.parallelism_analyse(vec![meeting_node_index])?;
+    //     }
+    //
+    //     Ok(())
+    // }
+    //
+    // /// check one node's children in a pipeline stream(children check only)
+    // /// the [`node_index`] must have only one parent
+    // /// = 0 child: stream finish
+    // /// = 1 child: pipeline stream, and go on recursion
+    // /// > 1 children:
+    // fn pipeline_parse(&mut self, node_index: NodeIndex) -> Result<Option<NodeIndex>, DagError> {
+    //     let stream_node = self.dag.index(node_index).clone();
+    //     let children: Vec<(EdgeIndex, NodeIndex)> =
+    //         self.dag.children(node_index).iter(&self.dag).collect();
+    //     let operator = {
+    //         let n = self.operators.get(&stream_node.id).unwrap();
+    //         &n.1
+    //     };
+    //
+    //     if children.len() == 0 {
+    //         return if operator.is_sink() {
+    //             Ok(None)
+    //         } else {
+    //             Err(DagError::ChildNodeNotFound(
+    //                 stream_node.operator_name.clone(),
+    //             ))
+    //         };
+    //     }
+    //
+    //     if children.len() == 1 {
+    //         let child = children.get(0).unwrap();
+    //         let parents: Vec<(EdgeIndex, NodeIndex)> =
+    //             self.dag.parents(child.1).iter(&self.dag).collect();
+    //
+    //         return if parents.len() == 1 {
+    //             let child_stream_index = self.dag.index_mut(child.1);
+    //             if child_stream_index.parallelism.value_type == ParValueType::Inherit {
+    //                 *child_stream_index.parallelism = *stream_node.parallelism;
+    //             }
+    //             self.pipeline_parse(child.1)
+    //         } else {
+    //             // cross node and return
+    //             Ok(Some(child.1))
+    //         };
+    //     }
+    //
+    //     if children.len() > 1 {
+    //         for child in children {
+    //             let parents: Vec<(EdgeIndex, NodeIndex)> =
+    //                 self.dag.parents(child.1).iter(&self.dag).collect();
+    //             let child_stream_node = self.dag.index_mut(child.1);
+    //             if child_stream_node.parallelism.value_type == ParValueType::Inherit {
+    //                 let max_parallelism = parents
+    //                     .iter()
+    //                     .map(|parent| self.dag.index(parent.1).parallelism)
+    //                     .max_by_key(|parallelism| *parallelism)
+    //                     .unwrap();
+    //                 *child_stream_node.parallelism = *max_parallelism;
+    //
+    //                 if parents.len() == 0 {}
+    //
+    //                 let n = if parents.len() == 1 {
+    //                     *child_stream_node.parallelism = *stream_node.parallelism;
+    //                     self.pipeline_parse(child.1)
+    //                 } else {
+    //                     if child_stream_node.parallelism.value_type == ParValueType::Inherit {}
+    //
+    //                     // cross node and return
+    //                     Ok(Some(child.1))
+    //                 };
+    //             }
+    //         }
+    //     }
+    //
+    //     unimplemented!()
+    // }
 }
