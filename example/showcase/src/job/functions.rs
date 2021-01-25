6dcff6735933494c774fedbbc4b0243d50a05ca2
yorkart <wangyue11.4@163.com>
2021-01-25 14:03:50 +0800

    showcase add Rand inputformat config Iterator change to block

--- example/showcase/src/job/functions.rs
+++ example/showcase/src/job/functions.rs
@@ -1,13 +1,69 @@
+use std::time::Duration;
+
 use chrono::{DateTime, Utc};
+use rand::Rng;
+use rlink::api;
 use rlink::api::element::Record;
 use rlink::api::function::{
     CoProcessFunction, Context, FilterFunction, FlatMapFunction, InputFormat, InputSplit,
     InputSplitSource,
 };
 use rlink::api::properties::Properties;
+use rlink::utils::date_time::current_timestamp_millis;
 
 use crate::buffer_gen::{config, model};
-use rlink::api;
+
+#[derive(Debug, Function)]
+pub struct RandInputFormat {}
+
+impl RandInputFormat {
+    pub fn new() -> Self {
+        RandInputFormat {}
+    }
+}
+
+impl InputSplitSource for RandInputFormat {}
+
+impl InputFormat for RandInputFormat {
+    fn open(&mut self, _input_split: InputSplit, _context: &Context) -> api::Result<()> {
+        Ok(())
+    }
+
+    fn record_iter(&mut self) -> Box<dyn Iterator<Item = Record> + Send> {
+        Box::new(RandIterator::new())
+    }
+
+    fn close(&mut self) -> api::Result<()> {
+        Ok(())
+    }
+}
+
+struct RandIterator {}
+
+impl RandIterator {
+    pub fn new() -> Self {
+        RandIterator {}
+    }
+}
+
+impl Iterator for RandIterator {
+    type Item = Record;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        std::thread::sleep(Duration::from_millis(1));
+        let mut thread_rng = rand::thread_rng();
+        let v = thread_rng.gen_range(0i32, 100i32) as i64;
+        let model = model::Entity {
+            timestamp: current_timestamp_millis(),
+            name: v.to_string(),
+            value: v,
+        };
+        let mut record = Record::new();
+        model.to_buffer(record.as_buffer()).unwrap();
+
+        Some(record)
+    }
+}
 
 #[derive(Debug, Function)]
 pub struct TestInputFormat {
@@ -169,11 +225,36 @@ impl InputFormat for ConfigInputFormat {
     }
 
     fn record_iter(&mut self) -> Box<dyn Iterator<Item = Record> + Send> {
-        Box::new(self.gen_row().into_iter())
+        Box::new(ConfigIterator::new(self.gen_row()))
     }
 
     fn close(&mut self) -> api::Result<()> {
         Ok(())
+    }
+}
+
+struct ConfigIterator {
+    conf: std::vec::IntoIter<Record>,
+}
+
+impl ConfigIterator {
+    pub fn new(conf: Vec<Record>) -> Self {
+        ConfigIterator {
+            conf: conf.into_iter(),
+        }
+    }
+}
+
+impl Iterator for ConfigIterator {
+    type Item = Record;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.conf.next() {
+            Some(record) => Some(record),
+            None => loop {
+                std::thread::sleep(Duration::from_secs(60));
+            },
+        }
     }
 }
 
