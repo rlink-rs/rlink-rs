333ec3f9ada82897a2f52b7d0cb7712bc17f48a2
yorkart <wangyue11.4@163.com>
2021-02-02 13:19:46 +0800

    set console as log default output

--- rlink/src/runtime/logger.rs
+++ rlink/src/runtime/logger.rs
@@ -1,49 +1,42 @@
-use std::str::FromStr;
+use std::path::PathBuf;
 
 use log::LevelFilter;
 use log::LevelFilter::Warn;
 use log4rs::append::console::{ConsoleAppender, Target};
-use log4rs::append::rolling_file::policy::compound::roll::fixed_window::FixedWindowRoller;
-use log4rs::append::rolling_file::policy::compound::trigger::size::SizeTrigger;
-use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
-use log4rs::append::rolling_file::RollingFileAppender;
 use log4rs::append::Append;
 use log4rs::config::{Appender, Config, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
 
-use crate::runtime::ClusterMode;
-use crate::utils::process::work_space;
-
-/// init log4r
-/// cluster_mode: for appender build
-/// level value: ["OFF", "ERROR", "WARN", "INFO", "DEBUG", "TRACE"], ignore ascii case
-pub(crate) fn init_log(cluster_mode: &ClusterMode, level: &str) {
-    let console = match cluster_mode {
-        ClusterMode::Local => true,
-        ClusterMode::Standalone => false,
-        ClusterMode::YARN => true,
+pub(crate) fn init_log(log_config_path: Option<String>) -> anyhow::Result<()> {
+    let config = match log_config_path {
+        Some(log_config_path) => {
+            let path = PathBuf::from(log_config_path);
+            load_config_from_file(path)?
+        }
+        None => init_default()?,
     };
 
-    let default_level = LevelFilter::from_str(level).expect("can not parse log level");
+    println!("{:?}", &config);
+    log4rs::init_config(config)?;
 
+    Ok(())
+}
+
+fn load_config_from_file(path: PathBuf) -> Result<Config, log4rs::Error> {
+    log4rs::load_config_file(path, Default::default())
+}
+
+fn init_default() -> Result<Config, log4rs::config::Errors> {
+    let name = "console";
+    let default_level = LevelFilter::Info;
     let encoder =
         PatternEncoder::new("{d(%Y-%m-%d %H:%M:%S%.3f)} {level} [{thread}] {target} - {m}{n}");
-
-    let (name, appender) = if !console {
-        ("rolling_file", create_rolling_file_appender(encoder))
-    } else {
-        ("console", create_console_appender(encoder))
-    };
-
-    let config = Config::builder()
+    let appender = create_console_appender(encoder);
+    Config::builder()
         .appender(Appender::builder().build(name, appender))
         .logger(Logger::builder().build("actix_web::middleware::logger", Warn))
         .logger(Logger::builder().build("clickhouse_rs", Warn))
         .build(Root::builder().appender(name).build(default_level))
-        .unwrap();
-
-    println!("{:?}", &config);
-    log4rs::init_config(config).unwrap();
 }
 
 fn create_console_appender(encoder: PatternEncoder) -> Box<dyn Append> {
@@ -52,28 +45,5 @@ fn create_console_appender(encoder: PatternEncoder) -> Box<dyn Append> {
         .encoder(Box::new(encoder))
         .build();
     let appender: Box<dyn Append> = Box::new(stdout);
-    appender
-}
-
-fn create_rolling_file_appender(encoder: PatternEncoder) -> Box<dyn Append> {
-    let cur_dir = work_space();
-    let path = cur_dir.join("task-manager.log");
-    let roll_path = path.to_str().unwrap().to_string() + ".{}";
-
-    let trigger = SizeTrigger::new(50 * 1024 * 1024);
-
-    let roll = FixedWindowRoller::builder()
-        .base(1)
-        .build(roll_path.as_str(), 20)
-        .expect("log roll error");
-
-    let policy = CompoundPolicy::new(Box::new(trigger), Box::new(roll));
-
-    let rolling_file = RollingFileAppender::builder()
-        .encoder(Box::new(encoder))
-        .append(true)
-        .build(path, Box::new(policy))
-        .unwrap();
-    let appender: Box<dyn Append> = Box::new(rolling_file);
     appender
 }
