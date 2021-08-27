022f06e933698daeec3fab5745ef4055c700a59b
yorkart <wangyue11.4@163.com>
2021-08-27 14:18:30 +0800

    set heartbeat check period 3s

--- rlink/src/runtime/coordinator/heart_beat_manager.rs
+++ rlink/src/runtime/coordinator/heart_beat_manager.rs
@@ -1,40 +1,22 @@
-use std::ops::Deref;
-use std::sync::RwLock;
 use std::time::Duration;
 
 use crate::core::cluster::MetadataStorageType;
-use crate::core::runtime::{ClusterDescriptor, ManagerStatus};
+use crate::core::runtime::ManagerStatus;
 use crate::storage::metadata::{loop_read_cluster_descriptor, MetadataStorage};
 use crate::utils;
-
-lazy_static! {
-    pub(crate) static ref CLUSTER_DESCRIPTOR: RwLock<Option<ClusterDescriptor>> = RwLock::new(None);
-}
 
 pub enum HeartbeatResult {
     Timeout,
     End,
 }
 
-fn update_global_cluster_descriptor(cluster_descriptor: ClusterDescriptor) {
-    let job_descriptor_rw: &RwLock<Option<ClusterDescriptor>> = &*CLUSTER_DESCRIPTOR;
-    let mut j = job_descriptor_rw.write().unwrap();
-    *j = Some(cluster_descriptor);
-}
-
-pub(crate) fn global_cluster_descriptor() -> Option<ClusterDescriptor> {
-    let job_descriptor_rw: &RwLock<Option<ClusterDescriptor>> = &*CLUSTER_DESCRIPTOR;
-    let j = job_descriptor_rw.read().unwrap();
-    j.deref().clone()
-}
-
+/// heartbeat timeout check
 pub(crate) fn start_heartbeat_timer(metadata_storage_mode: MetadataStorageType) -> HeartbeatResult {
     let metadata_storage = MetadataStorage::new(&metadata_storage_mode);
     loop {
-        std::thread::sleep(Duration::from_secs(5));
+        std::thread::sleep(Duration::from_secs(3));
 
         let cluster_descriptor = loop_read_cluster_descriptor(&metadata_storage);
-        update_global_cluster_descriptor(cluster_descriptor.clone());
 
         if cluster_descriptor.coordinator_manager.status == ManagerStatus::Terminated {
             return HeartbeatResult::End;
